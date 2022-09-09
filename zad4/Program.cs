6c9c98bb71ee4183138bbc1678e33f8742e4da67
kirill <kirillcava@mail.ru>
2022-09-09 14:26:52 +0300

    исправлено

--- zad4/Program.cs
+++ zad4/Program.cs
@@ -10,11 +10,11 @@ Console.Write("Введите третее число ");
 int num3 = Convert.ToInt32(Console.ReadLine());
 
 int max = num1;
-if (num2 > num1)
+if (num2 > max)
 {
     max = num2;
 }
-if (num3 > num2)
+if (num3 > max)
 {
     max = num3;
 }
